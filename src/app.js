c32b99fe54c29ee67c937838aef71b4e6a1d8e5a
caoyan <caoyan8817@gmail.com>
2021-05-22 21:08:49 +0200

    Added wind speed and humidity

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 function showTemp(response) {
   let temperature = Math.round(response.data.main.temp);
+  let humNumber = Math.round(response.data.main.humidity);
   let weatherDes = response.data.weather[0].description;
+  let windSpeed = Math.round(response.data.wind.speed);
+  let showHum = document.querySelector("span.humidity-percentage");
   let showCel = document.querySelector("span.temp");
-  let showDes = document.querySelector("#description");
+  let showDes = document.querySelector("#weather-description");
+  let showWindspeed = document.querySelector("span.wind-speed");
+  showWindspeed.innerHTML = `${windSpeed} m/s`;
+  showHum.innerHTML = `${humNumber} %`;
   showCel.innerHTML = temperature;
   showDes.innerHTML = weatherDes;
 }
